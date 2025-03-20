ff69272a3c97a7344e6bc0cfe0ba8c1790dcf157
Maame-boat <yboateng@constructor.university>
2025-03-20 22:36:12 +0100

    Add working Mult.asm and Fill.asm for homework

--- Boateng_Yaa/hw4/Mult.asm
+++ Boateng_Yaa/hw4/Mult.asm
@@ -7,40 +7,38 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-@2
-M=0
 
+M=0
 @0
 D=M
-@END
-D;
+@23
+D;JEQ
 
 @1
 D=M
-@END
+@23
 D;JEQ
 
 @0
 D=M
+
 @3
+
 M=D
-
-
-(LOOP)
 @1
 D=M
-
 @2
 M=D+M
 
 @3
+
 M=M-1
 
 D=M
-@LOOP
+
+@14
+
 D;JGT
 
-
-(END)
-@END
-
+@23
+0;JMP
