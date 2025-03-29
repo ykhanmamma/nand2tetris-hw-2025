0be8d800c84ca92f8f0c46a1c8da008d46e702e4
Slyrxc2exe <suadhuseynli11@gmail.com>
2025-03-29 16:50:40 +0100

    Huseynli_Suad_hw4

--- Huseynli_Suad/hw4/Fill.asm
+++ Huseynli_Suad/hw4/Fill.asm
@@ -9,3 +9,57 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+// Label for the start of the program
+(START)
+    @KBD
+    D=M        // Read the keyboard input
+    @DRAW_WHITE
+    D;JEQ      // If no key is pressed (D == 0), jump to DRAW_WHITE
+
+// If a key is pressed, fill the screen with black
+(DRAW_BLACK)
+    @SCREEN
+    D=A
+    @ADDR
+    M=D        // Initialize ADDR to point to SCREEN memory
+
+(LOOP_BLACK)
+    @ADDR
+    D=M        // Load the current address
+    @KBD
+    D=D-A
+    @START
+    D;JGE      // If we've reached the end of the screen memory, restart
+
+    @ADDR
+    A=M        // Set the address
+    M=-1       // Set the pixel to black (all 1s)
+
+    @ADDR
+    M=M+1      // Move to the next pixel
+    @LOOP_BLACK
+    0;JMP      // Repeat until the screen is filled
+
+// If no key is pressed, clear the screen (set all pixels to white)
+(DRAW_WHITE)
+    @SCREEN
+    D=A
+    @ADDR
+    M=D        // Initialize ADDR to point to SCREEN memory
+
+(LOOP_WHITE)
+    @ADDR
+    D=M        // Load the current address
+    @KBD
+    D=D-A
+    @START
+    D;JGE      // If we've reached the end of the screen memory, restart
+
+    @ADDR
+    A=M        // Set the address
+    M=0        // Set the pixel to white (all 0s)
+
+    @ADDR
+    M=M+1      // Move to the next pixel
+    @LOOP_WHITE
+    0;JMP      // Repeat until the screen is cleared
--- Huseynli_Suad/hw4/Mult.asm
+++ Huseynli_Suad/hw4/Mult.asm
@@ -8,3 +8,39 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+// Initialize R2 to 0 (this will store the result)
+@R2
+M=0
+
+// Load R0 (first number) into D
+@R0
+D=M
+@END
+D;JEQ       // If R0 is 0, jump to END (result is already 0)
+
+// Load R1 (second number) into counter
+@R1
+D=M
+@END
+D;JEQ       // If R1 is 0, jump to END (result is already 0)
+@COUNTER
+M=D         // COUNTER = R1 (number of times to add)
+
+// Loop to add R0 to R2, R1 times
+(LOOP)
+    @R0
+    D=M
+    @R2
+    M=M+D     // R2 = R2 + R0
+
+    // Decrement counter
+    @COUNTER
+    M=M-1
+    D=M
+    @LOOP
+    D;JGT     // If COUNTER > 0, repeat
+
+// End the program
+(END)
+@END
+0;JMP        // Infinite loop to stop execution
