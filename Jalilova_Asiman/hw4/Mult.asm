b778760a3676cc50c87cbe59387e29ce66d399ad
Asiman <ajalilova@constructor.university>
2025-03-29 14:13:09 +0100

    Added folder structure for homeworks

---
+++ Jalilova_Asiman/hw4/Mult.asm
@@ -0,0 +1,34 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// The algorithm is based on repetitive addition.
+
+(BEGIN)
+	// Initialize product to 0.
+	@R2
+	M=0
+(LOOP)
+	// If R1 <= 0, terminate.
+	@R1
+	D=M
+	@END
+	D;JLE
+	// Add R0 to R2.
+	@R0
+	D=M
+	@R2
+	M=D+M
+	// Decerement R1.
+	@R1
+	M=M-1
+	// Loop.
+	@LOOP
+	0;JMP
+(END)
+	// Terminate.
+	@END
+	0;JMP
