cca760d11d5028aa46dd98073e5073668c0a7459
Lass2005328 <ch.ilyess.23@gmail.com>
2025-03-30 22:03:15 +0200

    solved hw4

--- Chihaoui_Ilyess/hw4/Mult.asm
+++ Chihaoui_Ilyess/hw4/Mult.asm
@@ -1,15 +1,37 @@
-@R1
-    D=M
-    @LOOP
-    D;JGT
-(LOOP)
-    @R0
-    D=M
+// Start R2 at 0.
+@R2
+M=0
+
+// Jump into the first STEP if R0 > 0.
+@R0
+D=M
+@R2
+M=0
+
+@R0
+D=M
+@STEP
+D;JGT
+
+@END
+0;JMP
+(STEP)
     @R2
-    M=D+M
-    @R1
-    M=M-1
     D=M
-    @LOOP
+
+    @R1
+    D=D+M
+
+    @R2
+    M=D
+
+    @R0
+    D=M-1
+    M=D
+
+    @STEP
     D;JGT
+
 (END)
+    @END
+    0;JMP
