f544105e357a2dc16eb6e3fdf4382a9a6dea2807
Lass2005328 <ch.ilyess.23@gmail.com>
2025-03-30 01:55:12 +0100

    Solved hw4

--- Chihaoui_Ilyess/hw4/Fill.asm
+++ Chihaoui_Ilyess/hw4/Fill.asm
@@ -1,42 +1,48 @@
-// Fill.asm - Screen Blackout on Keypress
-
-// Define screen memory location
-@SCREEN
-D=M  // D = address of screen memory (beginning)
-
-// Infinite loop to check for key press
 (LOOP)
-    // Check if any key is pressed (KBD == 1)
     @KBD
-    D=M  // D = value from keyboard input (1 if a key is pressed, 0 otherwise)
+    D=M
+    @FILL_SCREEN
+    D;JNE
+    @CLEAR_SCREEN
+    0;JMP
 
-    // If a key is pressed, fill the screen with black (1)
-    @BLACK
-    D;JNE   // Jump to BLACK if a key is pressed
 
-    // If no key is pressed, fill the screen with white (0)
-    @WHITE
-    D;JEQ   // Jump to WHITE if no key is pressed
-
-(BLACK)
-    // Set every pixel on the screen to black (1)
+(FILL_SCREEN)
     @SCREEN
-    M=-1    // All pixels set to 1 (black)
+    D=A
+    @R0
+    M=D
 
-    // Wait for the key to be released (check KBD value)
-    @KBD
-    D=M     // Get the current value of the keyboard
-    @BLACK
-    D;JNE   // If the key is still pressed, stay in BLACK
-
+(FILL_LOOP)
+    @R0
+    A=M
+    M=-1
+    @R0
+    M=M+1
+    D=M
+    @24576
+    D=D-A
+    @FILL_LOOP
+    D;JLT
     @LOOP
-    0;JMP   // Repeat the loop if key is released
+    0;JMP
 
-(WHITE)
-    // Set every pixel on the screen to white (0)
+(CLEAR_SCREEN)
     @SCREEN
-    M=0     // All pixels set to 0 (white)
+    D=A
+    @R0
+    M=D
 
-    // Continue checking for key press
+(CLEAR_LOOP)
+    @R0
+    A=M
+    M=0
+    @R0
+    M=M+1
+    D=M
+    @24576
+    D=D-A
+    @CLEAR_LOOP
+    D;JLT
     @LOOP
-    0;JMP   // Continue the loop to check for key press
+    0;JMP
--- Chihaoui_Ilyess/hw4/Mult.asm
+++ Chihaoui_Ilyess/hw4/Mult.asm
@@ -1,33 +1,15 @@
-// Mult.asm - Repetitive Addition (Simple and Inefficient)
-
-// Inputs: 
-//  R0 = multiplicand (first number)
-//  R1 = multiplier (second number)
-
-// Output:
-//  R2 = result of multiplication (multiplicand * multiplier)
-
-(LOOP)
-    // Check if multiplier is zero, if so, end the loop
+@R1
     D=M
-    @END
-    D;JEQ
-
-    // Add multiplicand (R0) to the result (R2)
+    @LOOP
+    D;JGT
+(LOOP)
     @R0
     D=M
     @R2
     M=D+M
-
-    // Decrement multiplier (R1) by 1
     @R1
     M=M-1
-
-    // Repeat the loop
+    D=M
     @LOOP
-    0;JMP
-
+    D;JGT
 (END)
-    // End of program
-    @END
-    0;JMP
