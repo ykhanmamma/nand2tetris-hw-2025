8fc7b2c7b9700779e3fff10e35ca8797d59e259e
Deni355 <dkardjaliev@gmail.com>
2025-03-30 23:13:40 +0200

    Tried to fix Mult.asm loading problem

--- Kardzhaliev_Deniz/hw4/Mult.asm
+++ Kardzhaliev_Deniz/hw4/Mult.asm
@@ -1,26 +1,41 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
+
+@R2
+M=0
+
 @R0
 D=M
-@result
-M=0
+@END
+D;JEQ
+
 @R1
 D=M
-@LOOP
+@END
 D;JEQ
 
 (LOOP)
     @R0
     D=M
-    @result
-    M=M+D
-    @R1
-    MD=M-1
-    @LOOP
-    D;JGT
+    @END
+    D;JEQ
 
-(END)
-    @result
+    @R1
     D=M
     @R2
-    M=D
+    M=D+M
+
+    @R0
+    M=M-1
+
+    @LOOP
+    0;JMP
+
+(END)
     @END
     0;JMP
