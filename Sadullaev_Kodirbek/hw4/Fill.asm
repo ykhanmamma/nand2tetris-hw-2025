9f53ce3b3559fe2c17ad3c9a788ceab79bb30859
kody0801 <sadullayev0801@mail.ru>
2025-03-30 14:10:41 +0000

    3rd trytry

--- Sadullaev_Kodirbek/hw4/Fill.asm
+++ Sadullaev_Kodirbek/hw4/Fill.asm
@@ -1,55 +1,40 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+@24576
+D=A
+@keyboard
+M=D
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+(CHECK_KEYBOARD)
+@24575
+D=A
+@current
+M=D
 
-@color    // declare color variable
-M=0      // by default is white
+@keyboard
+A=M
+D=M
+@fillvalue
+M=-1
+@DRAW
+D;JNE
 
-(LOOP)
+@fillvalue
+M=0
 
-  @SCREEN
-  D=A
-  @pixels
-  M=D         // pixel address, goes from 16384 to 16384 + 8192 == 24576
+(DRAW)
+@fillvalue
+D=M
+@current
+A=M
+M=D
 
-  @KBD    // keyboard address
-  D=M
-  @BLACK
-  D;JGT     // if(keyboard > 0) goto BLACK
-  
-  @color
-  M=0       // set to white
-  @COLOR_SCREEN
-  0;JMP     // jump to subroutine that colors the screen
-  
-  (BLACK)
-    @color
-    M=-1    // set to black (2's complement 111111111...)
+@current
+D=M
+@16384
+D=D-A
+@CHECK_KEYBOARD
+D;JLE
 
-  (COLOR_SCREEN)
-    @color
-    D=M
-    @pixels
-    A=M         // VERY IMPORTANT! indirect address
-    M=D         // color M[pixels] with @color
-    
-    @pixels
-    M=M+1
-    D=M
-        
-    @24576
-    D=D-A
-    @COLOR_SCREEN
-    D;JLT
-
-@LOOP
-0;JMP // infinite loop
+@current
+M=M-1
+@DRAW
+0;JMP
--- Sadullaev_Kodirbek/hw4/Mult.asm
+++ Sadullaev_Kodirbek/hw4/Mult.asm
@@ -1,35 +1,31 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-
-// R2=0
-// while(R1 != 0)
-//   R2=R2+R0
+@R0
+D=M        // get R0
 
 @R2
-M=0
+M=0        // clear R2
+
+@R1
+D=M        // get R1
+@COUNT
+M=D        // set counter
+
 (LOOP)
-  @R1
-  D=M
-  @END
-  D;JEQ   // if(R1==0) goto END
-  
-  @R0
-  D=M       // D = R0
-  @R2
-  M=M+D     // R2 = R2+ R0
-  
-  @R1
-  M=M-1     // R1--
-  
-  @LOOP
-  0;JMP     // goto LOOP
+@COUNT
+D=M
+@END
+D;JEQ      // if 0, end
+
+@R0
+D=M
+@R2
+M=D+M      // add to R2
+
+@COUNT
+M=M-1      // dec counter
+
+@LOOP
+0;JMP      // loop
 
 (END)
-
-  @END
-  0;JMP // infinite loop
+@END
+0;JMP
