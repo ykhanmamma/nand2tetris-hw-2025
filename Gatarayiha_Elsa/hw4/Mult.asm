621f97bca643fe21f4cb41a48a37e6e427a84199
elsatara <elsatara10@gmail.com>
2025-03-30 22:23:36 +0200

    Fixed errors in mult.asm

--- Gatarayiha_Elsa/hw4/Mult.asm
+++ Gatarayiha_Elsa/hw4/Mult.asm
@@ -7,26 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
-@ i 
-M = 1
-@ 2 
-M = 0
+
+@i 
+M=1
+@2 
+M=0
 (LOOP)
-@ i //selects i
-D = M
-@ 1  
-D = D - A //setting limits
-@ END
-D; JGT   //if (i-RAM[1] > 0) goto END
-@ 0    //select value in RAM[0]
-D = M
-@ 2
-M = D + M   //stores the value of RAM[0] to itself
-@ i
-M = M + 1  //increase the iteration
+@i 
+D=M
+@1  
+D=D-A //setting limits
+@END
+D;JGT   //if (i-RAM[1] > 0) goto END
+@0    
+D=M
+@2
+M=D+M   //stores the value of RAM[0] to itself
+@i
+M=M+1  //increase the iteration
 @LOOP
-0; JMP //repeats the LOOP
+0;JMP //repeats the LOOP
 (END)
-@ END
+@END
 0;JMP
