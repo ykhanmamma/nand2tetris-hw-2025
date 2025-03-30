f6f6ef39d2549f9bffd3fecc4baeae23ebf6b7a8
yonuz812 <yonuz.sibel@yahoo.com>
2025-03-30 16:19:20 +0200

    HW4 Files

---
+++ Yonuz_Sibel/hw4/Mult.asm
@@ -0,0 +1,35 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// The algorithm is based on repetitive addition.
+
+//// Replace this comment with your code.
+
+(BEGIN)
+	@R2 //initialize to 0
+	M=0
+(LOOP)  //if R1 <= 0, exit
+	@R1
+	D=M
+	@END
+	D;JLE
+
+    //add R0 to R2
+	@R0
+	D=M
+	@R2
+	M=D+M
+
+
+	@R1		//decrement and loop until the value of R1 is 0
+	M=M-1
+	@LOOP
+	0;JMP
+
+(END)
+	@END
+	0;JMP
