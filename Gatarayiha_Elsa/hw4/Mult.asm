415ed176901f9e8bd0ab57e8ad28441c8fa52399
elsatara <elsatara10@gmail.com>
2025-03-30 22:32:00 +0200

    Update Mult.asm

--- Gatarayiha_Elsa/hw4/Mult.asm
+++ Gatarayiha_Elsa/hw4/Mult.asm
@@ -7,18 +7,23 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-
+@0
+D=A
 @i 
-M=1
+M=D
+
+@1
+D=M
 @2 
 M=0
+
 (LOOP)
 @i 
 D=M
 @1  
-D=D-A //setting limits
+D=D-M //setting limits
 @END
-D;JGT   //if (i-RAM[1] > 0) goto END
+D;JEQ   //if (i-RAM[1] > 0) goto END
 @0    
 D=M
 @2
@@ -27,6 +32,11 @@ M=D+M   //stores the value of RAM[0] to itself
 M=M+1  //increase the iteration
 @LOOP
 0;JMP //repeats the LOOP
+
 (END)
+@2
+D=M
+@1
+M=D
 @END
 0;JMP
